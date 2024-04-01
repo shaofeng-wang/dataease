4dc01570d8f2fb957417613aba261473a8a89f69
James Wang <shaofeng.wang@aliyun.com>
2024-04-01 09:53:04 +0800

    feat:增加版本信息

--- image.sh
+++ image.sh
@@ -1,15 +1,15 @@
-
 # mvn clean package
 
 export IMAGE_REPO=hospv3.test.yiyunhp.com:9443
 export IMAGE_TAG=1.18.7
+export IMAGE_BUILD=1002
 
 docker images | grep '/dataease/dataease-server' | awk '{print $3}' | xargs docker rmi
 
 docker buildx build \
-       --platform=linux/amd64 \
-       --build-arg IMAGE_TAG=$IMAGE_TAG \
-       -t $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG \
-       -o type=docker .
+  --platform=linux/amd64 \
+  --build-arg IMAGE_TAG=$IMAGE_TAG \
+  -t $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG.$IMAGE_BUILD \
+  -o type=docker .
 
-docker push $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG
+docker push $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG.$IMAGE_BUILD
--- image_deploy.sh
+++ image_deploy.sh
@@ -1,10 +1,11 @@
 export IMAGE_REPO=hospv3.test.yiyunhp.com:9443
 export IMAGE_TAG=1.18.7
+export IMAGE_BUILD=1002
 
 docker ps | grep '/dataease/dataease-server' | awk '{print $1}' | xargs docker stop | xargs docker rm
 
 docker images | grep '/dataease/dataease-server' | awk '{print $3}' | xargs docker rmi
 
-docker pull $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG
+docker pull $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG.$IMAGE_BUILD
 
-docker run -p 8081:8081 -v `pwd`/conf:/opt/dataease/conf -v `pwd`/data:/opt/dataease/data -dit --name dataease_server $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG
+docker run -p 8081:8081 -v `pwd`/conf:/opt/dataease/conf -v `pwd`/data:/opt/dataease/data -dit --name dataease_server $IMAGE_REPO/dataease/dataease-server:$IMAGE_TAG.$IMAGE_BUILD
